3b54264c1dda6594ee22822f1d722c3d17432051
barin <barin@BarinVM.myguest.virtualbox.org>
2023-09-15 14:03:16 +0300

    recovery: setup_fake_cache added support for f2fs

--- ramdisk-recovery-overlay/setup_fake_cache.sh
+++ ramdisk-recovery-overlay/setup_fake_cache.sh
@@ -1,20 +1,18 @@
-#!/bin/sh
-
-DATA_MOUNT_CODE=1
+#!/system/bin/sh
 
 if grep -qs '/data ' /proc/mounts; then
-       echo "userdata already mounted" >> /dev/kmsgs
-       DATA_MOUNT_CODE=0
+    echo "userdata already mounted" > /dev/kmsg
 else
-       while [ "$DATA_MOUNT_CODE" != "0" ]; do
-	       mount -t ext4 /dev/block/bootdevice/by-name/userdata /data 2> /dev/kmsgs
-	       DATA_MOUNT_CODE=$?
-	       sleep 1
-       done
+    # Timeout after 10s
+    for i in $(seq 1 10); do
+        mount -t ext4 /dev/block/bootdevice/by-name/userdata /data 2> /dev/kmsg && break
+        mount -t f2fs /dev/block/bootdevice/by-name/userdata /data 2> /dev/kmsg && break
+        sleep 1
+    done
 fi
 
-mkdir /data/cache >> /dev/kmsg
-mount -o bind /data/cache /cache >> /dev/kmsgs
+mkdir -p /data/cache > /dev/kmsg
+mount -o bind /data/cache /cache > /dev/kmsg
 
 setprop halium.datamount.done 1
 
