df0212c5b0bf8ce6a7f58b8008e3bfebd3d212fa
Ratchanan Srirattanamet <ratchanan@ubports.com>
2026-05-29 03:34:30 +0700

    Include recovery in rootfs part of device tarball and flash it on boot
    Due to a mistake in the previous recovery, the entry for the recovery
    partition was missing from the recovery's fstab, which means recovery
    can't update itsef.
    
    This commit adds a mechanism in which the recovery image is included in
    rootfs portion of the device tarball, which is then checked and flashed
    by `device-hacks` script.
    
    This allows recovery to be updated again, which in turn enables device
    to resize the rootfs partion.
    
    Bug: https://gitlab.com/ubports/development/core/hybris-support/lxc-android-config/-/work_items/54#note_3429872169
    (Jami: adapted from FP4)

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@ set -xe
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
 ./build/build.sh "$@"
+
+# Post-build steps to duplicate the built recovery into rootfs as well. This
+# will be used by `device-hacks` script to update recovery post-boot.
+# TODO: remove once 24.04-1.x goes EOL.
+
+./include-recovery-in-rootfs.sh "$@"
